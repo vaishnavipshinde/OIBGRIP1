dc97f5671e9e9e63e218007b9b75a09407c8f0c1
Vaishnavi Shinde <vpshinde9201@gmail.com>
2022-07-25 21:05:17 +0530

    UNIT CONVERTER APP
    First task of Level 1

---
+++ TASK 1/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.unitconverterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView2;
+    private EditText editText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView2 = findViewById(R.id.textView2);
+        editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = editText.getText().toString();
+                int kg = Integer.parseInt(s);
+                double pound = 2.20462 * kg;
+                textView2.setText("The pound value is" + pound);
+                Toast.makeText(MainActivity.this,"The value is" + pound,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
